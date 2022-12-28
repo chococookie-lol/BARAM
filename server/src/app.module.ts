4890427d28cfa2efbfdbb879113e36f692b91a1c
junhea <97426534+junhea@users.noreply.github.com>
2022-12-28 15:20:50 +0900

    feat: GET /matches/:matchId mock 구현

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SummonersModule } from './summoners/summoners.module';
+import { MatchesService } from './matches/matches.service';
+import { MatchesController } from './matches/matches.controller';
 
 @Module({
   imports: [SummonersModule],
-  controllers: [],
-  providers: [],
+  controllers: [MatchesController],
+  providers: [MatchesService],
 })
 export class AppModule {}
---
+++ server/src/matches/matches.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { MatchesService } from './matches.service';
+
+@Controller('/matches')
+export class MatchesController {
+  constructor(private readonly matchesService: MatchesService) {}
+  @Get(':matchId')
+  async findOne(@Param('matchId') matchId: number) {
+    return await this.matchesService.findOne(matchId);
+  }
+}
