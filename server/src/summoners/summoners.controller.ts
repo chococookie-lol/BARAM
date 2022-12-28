c88cdbe54fbb870eccdbd12cf23ac537f3b58d61
dolphinlmg <dolphinlmg@naver.com>
2022-12-28 15:00:12 +0900

    refactor: url 시작 / 추가

--- server/src/summoners/summoners.controller.ts
+++ server/src/summoners/summoners.controller.ts
@@ -5,12 +5,12 @@ import { SummonersService } from './summoners.service';
 export class SummonersController {
   constructor(private readonly summonersService: SummonersService) {}
 
-  @Get(':userName')
+  @Get('/:userName')
   async findOne(@Param('userName') userName: string) {
     return await this.summonersService.findOne(userName);
   }
 
-  @Post(':userName')
+  @Post('/:userName')
   async update(@Param('userName') userName: string) {
     return await this.summonersService.update(userName);
   }
