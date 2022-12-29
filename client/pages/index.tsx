59d046c61d10fcbe5e3971529ce39b7cfa934188
junhea <97426534+junhea@users.noreply.github.com>
2022-12-30 07:52:11 +0900

    refactor: revert pages/index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,16 +1,3 @@
-import Pie from '../components/Doughnut';
-import { useGlobalTheme } from '../styles/GlobalThemeContext';
-
 export default function Home() {
-  const context = useGlobalTheme();
-  return (
-    <div>
-      <Pie
-        val={[5551, 2322, 200]}
-        title="딜 유형"
-        label={['마법', '물리', '고정']}
-        color={[context.theme.blue1, context.theme.red1, context.theme.background]}
-      />
-    </div>
-  );
+  return <div>Hello World!</div>;
 }
