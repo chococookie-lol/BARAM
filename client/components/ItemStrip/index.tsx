c61ba00cebdf88dd32558353c4337bd68375378e
junhea <97426534+junhea@users.noreply.github.com>
2023-01-03 14:59:59 +0900

    feat: ItemStrip 구현

---
+++ client/components/ItemStrip/index.tsx
@@ -0,0 +1,36 @@
+import { css } from '@emotion/react';
+import ItemIcon from '../ItemIcon';
+
+interface ItemStripProps {
+  items: number[];
+  version: string;
+  width: number;
+  height: number;
+  padding?: number;
+}
+
+function ItemStrip({ items, version, width, height, padding = 3 }: ItemStripProps) {
+  const nullItem = 7050;
+  return (
+    <div
+      css={css`
+        display: flex;
+      `}
+    >
+      {items.map((e, i) => {
+        return (
+          <div
+            key={i}
+            css={css`
+              padding-left: ${padding}px;
+            `}
+          >
+            <ItemIcon id={e ? e : nullItem} version={version} width={width} height={height} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ItemStrip;
