86250a9453c71f51b18f21c137a0cced5180b4f7
dolphinlmg <dolphinlmg@naver.com>
2023-01-11 11:34:00 +0900

    feat: Riot API 모듈 생성

---
+++ server/src/riot.api/interface/index.ts
@@ -0,0 +1,1 @@
+export * from './riot.summoner.interface';
---
+++ server/src/riot.api/interface/riot.summoner.interface.ts
@@ -0,0 +1,9 @@
+export interface RoitSummonerResponse {
+  id: string;
+  accountId: string;
+  puuid: string;
+  name: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+}
