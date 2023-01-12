2286e59acdcf3d44e1dc1db0e3ed50b1a12dee85
junhea <97426534+junhea@users.noreply.github.com>
2023-01-12 23:54:00 +0900

    feat: GET /matches/:matchId 구현

--- server/src/matches/matches.module.ts
+++ server/src/matches/matches.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RiotApiModule } from 'src/riot.api/riot.api.module';
 import { MatchesController } from './matches.controller';
 import { MatchesService } from './matches.service';
+import { Match, matchSchema } from './schemas/match.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Match.name, schema: matchSchema }]), RiotApiModule],
   controllers: [MatchesController],
   providers: [MatchesService],
 })
