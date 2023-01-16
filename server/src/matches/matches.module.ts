bc14a1cf5b52a61e97d606f2b0946a5333784c1c
junhea <97426534+junhea@users.noreply.github.com>
2023-01-16 14:27:39 +0900

    feat: match fetch시 polling 가능 하도록 수정

--- server/src/matches/matches.module.ts
+++ server/src/matches/matches.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { RiotApiModule } from 'src/riot.api/riot.api.module';
+import { Summoner, SummonerSchema } from 'src/summoners/schemas/summoner.schema';
 import { MatchesController } from './matches.controller';
 import { MatchesService } from './matches.service';
 import { Match, matchSchema } from './schemas/match.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Match.name, schema: matchSchema }]), RiotApiModule],
+  imports: [
+    MongooseModule.forFeature([{ name: Match.name, schema: matchSchema }]),
+    MongooseModule.forFeature([{ name: Summoner.name, schema: SummonerSchema }]),
+    RiotApiModule,
+  ],
   controllers: [MatchesController],
   providers: [MatchesService],
 })
