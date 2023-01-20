dc7ece7a07a068b4389a349ebd640eee22080868
dolphinlmg <dolphinlmg@naver.com>
2023-01-20 15:09:23 +0900

    feat: font 적용
    - NanumSquare

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ko">
-      <Head />
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
