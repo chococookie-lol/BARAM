960ca2cb6a6fb5474ab16cc9125909c9f76c9e71
dolphinlmg <dolphinlmg@naver.com>
2023-01-27 18:18:35 +0900

    refactor: summoner mock 재활용

--- server/src/summoners/test/summoners.controller.spec.ts
+++ server/src/summoners/test/summoners.controller.spec.ts
@@ -1,47 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { SummonersController } from '../summoners.controller';
 import { SummonersService } from '../summoners.service';
+import { mockSummoner } from './summoners.mock';
 
 describe('SummonersController', () => {
   let controller: SummonersController;
   let service: SummonersService;
-
-  const mockSummoner = {
-    _id: '63c606679355463463e669d5',
-    puuid: 'xF6UfEzvrs89bWB3PhtBLKt74bthXbc2QQGY-8YlgcSvmvVw0SOjvM5zMXT6YpSMhsST65BMj0jFlw',
-    __v: 0,
-    challenges: [
-      {
-        challengeId: 2022001,
-        percentile: 0.006,
-        level: 'MASTER',
-        value: 28,
-        achievedTime: 1660908685089,
-        _id: '63c78c013b96d87cb690268b',
-      },
-      {
-        challengeId: 101101,
-        percentile: 0,
-        level: 'GRANDMASTER',
-        value: 128,
-        achievedTime: 1656421103633,
-        _id: '63c78c013b96d87cb690268c',
-      },
-      {
-        challengeId: 101106,
-        percentile: 0.007,
-        level: 'MASTER',
-        value: 8,
-        achievedTime: 1666795991304,
-        _id: '63c78c013b96d87cb690268d',
-      },
-    ],
-    isFetching: false,
-    level: 123,
-    name: 'dolphinlmg',
-    profileIconId: 5528,
-    updatedAt: '2023-01-25T07:51:18.986Z',
-  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
