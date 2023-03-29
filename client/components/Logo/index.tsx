c36635ecbb1bc0b0db6f471265f89b45684c9456
dolphinlmg <dolphinlmg@naver.com>
2023-03-29 18:16:43 +0900

    ✨ feat: 로고 클릭시 메인화면으로 이동하도록 구현
    Close #285

--- client/components/Logo/index.tsx
+++ client/components/Logo/index.tsx
@@ -1,4 +1,6 @@
 import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { useGlobalTheme } from '../../styles/GlobalThemeContext';
 import LogoSvg from '/assets/logo.svg';
 
@@ -9,13 +11,23 @@ interface LogoProps {
 
 export default function Logo({ width, height }: LogoProps) {
   const context = useGlobalTheme();
+  const router = useRouter();
+
   return (
-    <LogoSvg
-      css={css`
-        ${width ? `width: ${width}px;` : null}
-        ${height ? `height: ${height}px;` : null}
-      `}
-      fill={context.theme.accent1}
-    />
+    <div
+      onClick={() => {
+        if (router.isReady) {
+          router.push('/');
+        }
+      }}
+    >
+      <LogoSvg
+        css={css`
+          ${width ? `width: ${width}px;` : null}
+          ${height ? `height: ${height}px;` : null}
+        `}
+        fill={context.theme.accent1}
+      />
+    </div>
   );
 }
