2856447f497989c06d501c309a514d09615f49c6
dolphinlmg <dolphinlmg@naver.com>
2023-08-28 17:10:18 +0900

    feat: emotion theme 타입 설정

---
+++ client/styles/emotion.d.ts
@@ -0,0 +1,20 @@
+import '@emotion/react';
+
+declare module '@emotion/react' {
+  export interface Theme {
+    background: string;
+    foreground: string;
+    accent1: string;
+    accent2: string;
+    accent3: string;
+    blue1: string;
+    blue2: string;
+    blue3: string;
+    blue4: string;
+    red1: string;
+    red2: string;
+    red3: string;
+    red4: string;
+    neutral: string;
+  }
+}
